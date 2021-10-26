31b64cef54ce3aff5d040dafa07062bacf4ade46
Gregory Delgado <gregd92694@yahoo.com>
2021-10-26 16:21:45 -0500

    Completed problem set 4.

--- script.js
+++ script.js
@@ -25,3 +25,30 @@ for(let i =0;i<a.length;i++){
 for(let x =a.length; x>=0;x--){
     console.log(a.charAt(x))
 }
+
+//Capitalizing first let of words
+let b= "hello world";
+let b2= b.charAt(0).toUpperCase()+b.slice(1);
+console.log(b2)
+
+//Compressing string of characters
+const str1 ='aaaaccccbbdddd'
+const str2 ='hhhjjjjkkkkklllll'
+const str3 ='mmmmmnnnnnnfffff'
+const compressString=(str='')=>{
+    let res ='';
+    let count =1;
+    for(let i=0;i<str.length;i++){
+        let cur=str[i];
+        let next =str[i+1];
+        if(cur===next){
+            count++;
+        }
+        else{res +=cur+String(count);
+        count=1;}
+    };
+    return res.length<str.length?res:str;
+};
+console.log(compressString(str1));
+console.log(compressString(str2));
+console.log(compressString(str3));
