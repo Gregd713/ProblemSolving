7e99be8e209735cad14307eb540206dad0f67206
Gregory Delgado <gregd92694@yahoo.com>
2021-10-26 14:17:39 -0500

    initial

---
+++ script.js
@@ -0,0 +1,20 @@
+"Use Strict";
+
+// loop counting to 100
+//loop declaring even or odd
+for(var i = 0; i <=100; i++){
+    if(i%3===0&&i%5===0){
+        console.log(i+" Fizz Buzz");
+    }
+    else if (i %3===0){
+        console.log(i+" Fizz");}
+    else if (i%5===0){
+        console.log(i+" Buzz");
+    }
+    else{
+        console.log(i)
+    }
+}
+function divisibleByTwoNum(i){
+
+}
